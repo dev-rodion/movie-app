4e0e84fc8a4f18a449c11f2dd83900ea9916a4bd
dev-rodion <gudkov.rodion.21@gmail.com>
2022-03-22 12:09:27 +0200

    Created Footer component

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -1,7 +1,43 @@
 import React from "react";
 
+import "./footer.scss";
+
+import { Link } from "react-router-dom";
+
+import bg from "../../assets/footer-bg.jpg";
+import logo from "../../assets/tmovie.png";
+
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <footer className="footer" style={{ backgroundImage: `url(${bg})` }}>
+      <div className="footer__content container">
+        <div className="footer__logo logo">
+          <img src={logo} alt="tMovies Logo" />
+          <Link to="/">tMovies</Link>
+        </div>
+
+        <div className="footer__menus">
+          <nav className="footer__menu">
+            <Link to="/">Home</Link>
+            <Link to="/">Contact Us</Link>
+            <Link to="/">Term of Services</Link>
+            <Link to="/">About Us</Link>
+          </nav>
+          <nav className="footer__menu">
+            <Link to="/">Live</Link>
+            <Link to="/">FAQ</Link>
+            <Link to="/">Premium</Link>
+            <Link to="/">Privacy Policy</Link>
+          </nav>
+          <nav className="footer__menu">
+            <Link to="/">You must watch</Link>
+            <Link to="/">Recent release</Link>
+            <Link to="/">Top IMDB</Link>
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
 
 export default Footer;
--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -46,7 +46,7 @@ const Header = () => {
     <div ref={headerRef} className="header">
       <div className="header__wrap container">
         <div className="logo">
-          <img src={logo} alt="" />
+          <img src={logo} alt="tMovies Logo" />
           <Link to="/">tMovies</Link>
         </div>
 
