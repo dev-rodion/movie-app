efafd92a8090258e290ee1ceed9cd6098e484b4b
dev-rodion <gudkov.rodion.21@gmail.com>
2022-03-26 15:56:18 +0200

    Added MovieGrid component. Updated Catalog Page

--- src/pages/Catalog.js
+++ src/pages/Catalog.js
@@ -3,6 +3,7 @@ import { useParams } from "react-router-dom";
 import PageHeader from "../components/page-header/PageHeader";
 
 import { category as cate } from "../api/tmdbApi";
+import MovieGrid from "../components/movie-grid/MovieGrid";
 
 const Catalog = () => {
   const { category } = useParams();
@@ -10,6 +11,12 @@ const Catalog = () => {
   return (
     <>
       <PageHeader>{category === cate.movie ? "Movies" : "TV Series"}</PageHeader>
+
+      <div className="container">
+        <div className="section mb-3">
+          <MovieGrid category={category} />
+        </div>
+      </div>
     </>
   );
 };
